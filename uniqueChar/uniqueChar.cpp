32d32624ef968ff0450601caf75eb8345342480a
Matei, Ethan Laurentiu <ematei@iu.edu>
2024-07-03 16:33:02 -0400

    Unique Char

---
+++ uniqueChar/uniqueChar.cpp
@@ -0,0 +1,19 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map <char , int> check;
+        for (char i: s){
+            check[i]++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(check[s[i]]==1) return i;
+        }
+        return -1;
+
+        
+    }    
+};
